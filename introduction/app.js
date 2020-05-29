76656a7884d045c20c25de69d8e00f3f0048c345
Arsene Kevin <kevinkouomeu@icloud.com>
2020-05-29 18:41:13 +0200

    Express.js : The basics ( working with middlewares next() )

--- introduction/app.js
+++ introduction/app.js
@@ -1,2 +1,20 @@
 // Express JS is Backend web framework for NodeJS
 // node install express --save (for production dependency)
+
+const express = require("express");
+
+const app = express();
+
+// First middleware
+app.use((request, response, next) => {
+  response.setHeader("Content-Type", "text/html");
+  //use next() to tell express not to stop the request here and continue to the next middleware
+  next();
+});
+
+//second middleware
+app.use((request, response, next) => {
+  response.send("<h1>Hello world !</h1>");
+});
+
+app.listen(3000);
