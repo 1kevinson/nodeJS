eedeb48d49f4b4ca571ab8bf63879e30f3e71d85
Arsene Kevin <kevinkouomeu@icloud.com>
2020-05-29 18:51:16 +0200

    Extracting Data

--- introduction/app.js
+++ introduction/app.js
@@ -2,19 +2,28 @@
 // node install express --save (for production dependency)
 
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 // First middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use((request, response, next) => {
   response.setHeader("Content-Type", "text/html");
   //use next() to tell express not to stop the request here and continue to the next middleware
   next();
 });
 
-//second middleware
 app.use((request, response, next) => {
-  response.send("<h1>Hello world !</h1>");
+  let userName = request.body.username || "unknown user";
+  response.send(
+    `<h1>Hi ${userName}</h1>` +
+      "<form method='post' action='/'>" +
+      "<input name='username' type='text'>" +
+      "<button type='submit'> Send </button>" +
+      "</form>"
+  );
 });
 
 app.listen(3000);
